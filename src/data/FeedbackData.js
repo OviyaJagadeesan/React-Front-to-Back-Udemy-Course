bed837fd711c0d94490345deec8078d3d1532061
Oviya <110013090+OviyaJagadeesan@users.noreply.github.com>
2023-04-19 12:59:51 +0530

    First Commit

---
+++ src/data/FeedbackData.js
@@ -0,0 +1,19 @@
+const FeebackData = [
+    {
+        id: 1,
+        rating:10,
+        text:"Good work"
+      },
+      {
+        id: 2,
+        rating:9,
+        text:"Okay"
+      },
+      {
+        id: 3,
+        rating:8,
+        text:"Thank You"
+      },
+]
+
+export default FeebackData;
